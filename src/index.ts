351d54c88b7aae397e346e852ed8eceaf06c5d0b
cominoapp <cs@comino.app>
2025-10-24 23:31:48 +0900

    Add Vercel URL to CORS

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://otonaba-frontend.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
