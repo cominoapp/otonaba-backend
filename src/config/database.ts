bff712e1360ce18482164b38e51a238c238ff25f
cominoapp <cs@comino.app>
2025-10-24 22:38:25 +0900

    Enable SSL for database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,9 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const connectDB = async () => {
